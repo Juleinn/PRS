3182bb51fa262a257e762191208c2de5c88ada04
BlutHanfling <squirkiz@gmail.com>
2017-04-12 16:30:39 +0200

    Ajout data_load.c

--- src/common/data_load.c
+++ src/common/data_load.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
 #include "data_load.h"
+
+
+void * data_load(void * data)
+{
+  //Init
+  DataLoad_Data* dataUse = data;
+  printf("%s\n", dataUse->filename);
+
+  FILE *fp;
+  fp = file = fopen(dataUse->filename, "r");
+
+  while(1)
+  {
+
+    //Lock mutex (writing in process...)
+    fread(dataUse->buffer[0+CHUNK_SIZE*0], CHUNK_SIZE, 1, fp);
+    //Unlock mutex (reading in process)
+  }
+
+ return data;
+}
